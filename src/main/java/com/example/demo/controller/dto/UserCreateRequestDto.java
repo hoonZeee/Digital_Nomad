bcb0b3a402550a6776539bb611c182f1e81b6773
Jihoon <dlwlgns7540@naver.com>
2025-07-10 23:28:31 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/dto/UserCreateRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserCreateRequestDto {
+    private String username;
+    private String password;
+    private String name;
+    private Integer age;
+    private String job;
+    private String specialty;
+}
